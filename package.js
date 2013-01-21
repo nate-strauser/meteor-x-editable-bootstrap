d3973373685adcddb73e52b2131114efa5558a50
Nathan Strauser <nathan.strauser@gmail.com>
2013-01-21 15:34:38 -0500

    path fix

--- package.js
+++ package.js
@@ -18,4 +18,7 @@ Package.on_use(function (api){
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysiwyg-color.css', 'client');
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client');
 	api.add_files('lib/wysihtml5/wysihtml5.js', 'client');  
+	
+	//override image paths
+	api.add_files('path-override.css', 'client');  
 });
