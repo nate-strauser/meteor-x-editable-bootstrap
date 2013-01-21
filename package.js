c2034ae0e8eb61ae9baf36b81115c5c4e36ecd89
Nathan Strauser <nathan.strauser@gmail.com>
2013-01-21 15:14:48 -0500

    initial commit

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+	summary: "Latest version of X-Editable for Bootstrap with wysihtml5 rich text editor"
+});
+
+Package.on_use(function (api){
+	//x-editable
+	api.add_files('lib/bootstrap-editable/css/bootstrap-editable.css', 'client');
+	api.add_files('lib/bootstrap-editable/js/bootstrap-editable.js', 'client');
+	api.add_files('lib/bootstrap-editable/img/clear.png', 'client');
+	api.add_files('lib/bootstrap-editable/img/loading.gif', 'client');
+	
+	//address
+	api.add_files('lib/address/address.css', 'client');
+	api.add_files('lib/address/address.js', 'client');
+
+	//wysihtml5
+	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.css', 'client');
+	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysiwyg-color.css', 'client');
+	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client');
+	api.add_files('lib/wysihtml5/wysihtml5.js', 'client');  
+});
