0396cd3363fef002b8c7adf6615774d4d820f1df
Nathan Strauser <nathan.strauser@gmail.com>
2013-09-23 16:02:29 -0400

    client bare load

--- package.js
+++ package.js
@@ -8,20 +8,20 @@ Package.on_use(function (api){
 
 	//x-editable
 	api.add_files('lib/bootstrap-editable/css/bootstrap-editable.css', 'client');
-	api.add_files('lib/bootstrap-editable/js/bootstrap-editable.js', 'client');
+	api.add_files('lib/bootstrap-editable/js/bootstrap-editable.js', 'client', {bare:true});
 	api.add_files('lib/bootstrap-editable/img/clear.png', 'client');
 	api.add_files('lib/bootstrap-editable/img/loading.gif', 'client');
 	
 	//address
 	api.add_files('lib/address/address.css', 'client');
-	api.add_files('lib/address/address.js', 'client');
+	api.add_files('lib/address/address.js', 'client', {bare:true});
 
 	//wysihtml5
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysiwyg-color.css', 'client');
-	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client');
+	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client', {bare:true});
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.css', 'client');
-	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.js', 'client');
-	api.add_files('lib/wysihtml5/wysihtml5.js', 'client');  
+	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.js', 'client', {bare:true});
+	api.add_files('lib/wysihtml5/wysihtml5.js', 'client', {bare:true});  
 	
 	//override image paths
 	api.add_files('path-override.css', 'client');  
