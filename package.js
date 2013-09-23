18b37e5f42400480a007c5242e9d6adc780e093d
Nathan Strauser <nathan.strauser@gmail.com>
2013-09-23 16:39:30 -0400

    wrapping

--- package.js
+++ package.js
@@ -17,11 +17,11 @@ Package.on_use(function (api){
 	api.add_files('lib/address/address.js', 'client', {bare:true});
 
 	//wysihtml5
+	api.add_files('lib/wysihtml5/wysihtml5.js', 'client', {bare:true});  
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysiwyg-color.css', 'client');
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client', {bare:true});
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.css', 'client');
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.js', 'client', {bare:true});
-	api.add_files('lib/wysihtml5/wysihtml5.js', 'client', {bare:true});  
 	
 	//override image paths
 	api.add_files('path-override.css', 'client');  
